8aaed43e2130980c12c1ce5647c3600177254d40
Hirosolo <ntaivt2004@gmail.com>
2025-09-21 04:43:36 +0700

    add sepolia testnet network

--- src/app/components/marketplace/header.tsx
+++ src/app/components/marketplace/header.tsx
@@ -132,7 +132,7 @@ export default function MarketplaceSearchBar() {
                             )}
                           </div>
                         )}
-                        {chain.name}
+                        <span className="text-moss-900">{chain.name}</span>
                       </button>
 
                       <button
