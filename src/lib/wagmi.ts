27ce9f21f43eb8222188266360aebdc4b0cb6d88
Trong-Tra <traminhtrong2604@gmail.com>
2025-09-21 15:33:30 +0700

    build: add Base Sepolia testnet configuration to wagmi

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,10 +1,29 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { mainnet, sepolia, polygon, arbitrum } from 'wagmi/chains';
+// Base Sepolia testnet config
+export const baseSepolia = {
+  id: 84532,
+  name: 'Base Sepolia',
+  network: 'base-sepolia',
+  nativeCurrency: {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: { http: ['https://sepolia.base.org'] },
+    public: { http: ['https://sepolia.base.org'] },
+  },
+  blockExplorers: {
+    default: { name: 'Basescan', url: 'https://sepolia.basescan.org' },
+  },
+  testnet: true,
+};
 
 export const config = getDefaultConfig({
   appName: "Landzen",
   projectId: "5c992b50e8871ed27bdb8e9f96975888",
-  chains: [mainnet, sepolia, polygon, arbitrum],
+  chains: [mainnet, sepolia, polygon, arbitrum, baseSepolia],
   ssr: true,
 });
 
