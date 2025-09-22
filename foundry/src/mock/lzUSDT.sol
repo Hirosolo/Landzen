b997dce98f6a4f7eb1be7c16cf0897bc8b6f01a2
Trong-Tra <traminhtrong2604@gmail.com>
2025-09-22 18:08:42 +0700

    fix: update constructor parameters for lzUSDT contract to reflect correct token name and symbol

--- foundry/src/mock/lzUSDT.sol
+++ foundry/src/mock/lzUSDT.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract lzUSDT is ERC20, Ownable {
-    constructor(address owner) ERC20("Mock Tether USD", "mUSDT") Ownable(owner) {
+    constructor(address owner) ERC20("LandZen USD Token", "lzUSDT") Ownable(owner) {
         _mint(owner, 1_000_000 * 10**decimals());
     }
     
