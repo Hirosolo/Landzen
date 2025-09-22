a2e18cf55c756980d1d7ffc1d08256c14f55502f
Trong-Tra <traminhtrong2604@gmail.com>
2025-09-22 10:40:25 +0700

    feat: add lzUSDT contract with minting functionality

---
+++ foundry/src/mock/lzUSDT.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract lzUSDT is ERC20, Ownable {
+    constructor(address owner) ERC20("Mock Tether USD", "mUSDT") Ownable(owner) {
+        _mint(owner, 1_000_000 * 10**decimals());
+    }
+    
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
