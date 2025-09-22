28fb4e48b10b2e4fe756652439b000a1b62e1bea
Trong-Tra <traminhtrong2604@gmail.com>
2025-09-22 17:21:03 +0700

    feat: add deployment script for lzUSDT contract

---
+++ foundry/script/deploy_lzUSDT.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "forge-std/Script.sol";
+import "../src/mock/lzUSDT.sol";
+
+contract DeployLzUSDT is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.addr(deployerPrivateKey);
+
+        vm.startBroadcast();
+
+        lzUSDT lzUsdt = new lzUSDT(deployer);
+        console.log("lzUSDT deployed to:", address(lzUsdt));
+        
+        vm.stopBroadcast();
+    }
+}
