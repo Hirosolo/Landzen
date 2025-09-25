b9b2aab40ef676da567a1ce7c4851e23022087a4
Trong-Tra <traminhtrong2604@gmail.com>
2025-09-25 16:54:46 +0700

    refactor: add mockStableToken contract with minting functionality

--- foundry/src/mock/mockStableToken.sol
+++ foundry/src/mock/mockStableToken.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract lzUSDT is ERC20, Ownable {
-    constructor(address owner) ERC20("LandZen USD Token", "lzUSDT") Ownable(owner) {
+contract mockStableToken is ERC20, Ownable {
+    constructor(address owner, string memory _tokenName, string memory _tokenSymbol) 
+    ERC20(_tokenName, _tokenSymbol) Ownable(owner) {
         _mint(owner, 1_000_000 * 10**decimals());
     }
     
