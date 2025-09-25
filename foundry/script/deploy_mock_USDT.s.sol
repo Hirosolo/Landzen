ab6eaaadffb46d376424a4d61431abcab86b56e0
Trong-Tra <traminhtrong2604@gmail.com>
2025-09-25 16:56:26 +0700

    refactor: add DeployLzUSDT script for deploying mock USDT token

--- foundry/script/deploy_mock_USDT.s.sol
+++ foundry/script/deploy_mock_USDT.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.26;
 
 import "forge-std/Script.sol";
-import "../src/mock/lzUSDT.sol";
+import "../src/mock/mockStableToken.sol";
 
 contract DeployLzUSDT is Script {
     function run() external {
@@ -11,8 +11,9 @@ contract DeployLzUSDT is Script {
 
         vm.startBroadcast(deployerPrivateKey);
 
-        lzUSDT lzUsdt = new lzUSDT(deployer);
-        console.log("lzUSDT deployed to:", address(lzUsdt));
+         
+        mockStableToken usdt = new mockStableToken(deployer, "Tether USD", "USDT");
+        console.log("Deployed mock USDT at:", address(usdt));
         
         vm.stopBroadcast();
     }
